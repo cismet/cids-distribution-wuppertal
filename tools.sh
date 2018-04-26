699d53accc638962839066de8135d85b46e44292
JeanAtCismet <jean.ruiz@cismet.de>
2018-04-26 16:28:58 +0200

    adding home to tools volumes

--- tools.sh
+++ tools.sh
@@ -5,6 +5,7 @@ PARAMS=$@
 docker run --rm -it \
   -e CIDS_DISTRIBUTION_DIR=${DIR} \
   -v ${DIR}:${DIR}:ro \
+  -v $HOME:/root:ro \
   -v /var/run/docker.sock:/var/run/docker.sock \
   reg.cismet.de/abstract/cids-distribution-tools:18.04.1 \
   ${PARAMS}
